934cee75dee2a0e594724685bc493b34f467ae7d
Michael Primavera <pastasauce59@gmail.com>
2021-03-24 22:04:17 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+ 
+const names = []
+
+function writeCards(name, surprise){
+    for (let i = 0; i < name.length; i++) {
+        names.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+}
+
+    return names;
+}
+
+function countDown(){
+    let countdown = 10;
+    while (0 <= countdown) {
+        console.log(countdown--);
+    
+    }
+
+  return countDown
+}
+
+
+
+
+
